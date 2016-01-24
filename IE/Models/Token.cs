dcd3068f1b58c8cdf3d57910552c3cc98ea8b81a
John Paul Yao <yaojohnpaul@gmail.com>
2016-01-24 13:23:25 +0800

    Revamped to follow the UML

---
+++ IE/Models/Token.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IE.Models
+{
+    enum NamedEntity
+    {
+        PERSON,
+        LOCATION,
+        DATE,
+        ORGANIZATION,
+        TIME
+    }
+
+    enum PartOfSpeech
+    {
+        NOUN,
+        ADJECTIVE,
+        VERB
+    }
+
+    class Token
+    {
+        public String Value { get; set; }
+
+        public int Sentence { get; set; }
+
+        public int Position { get; set; }
+
+        public PartOfSpeech PartOfSpeech { get; set; }
+
+        public NamedEntity NamedEntity { get; set; }
+
+        public int Frequency { get; set; }
+
+        public Boolean IsWho { get; set; }
+
+        public Boolean IsWhen { get; set; }
+
+        public Boolean IsWhere { get; set; }
+
+        public Boolean IsWhat { get; set; }
+
+        public Boolean IsWhy { get; set; }
+
+        public Token(String value, int position)
+        {
+            Value = value;
+            Position = position;
+        }
+    }
+}
